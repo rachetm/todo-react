84254a8f98fb4f62a2519515c08db19ccc269edf
Rachet Mudnur <rmudnur@gmail.com>
2020-02-19 18:07:26 +0530

    Changed to grid layout and used IconButton component

--- src/components/Items/item/item.js
+++ src/components/Items/item/item.js
@@ -1,28 +1,43 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 
 class Item extends React.Component {
   render() {
     return (
-      <div className="item-container">
-        <Checkbox
-          color="primary"
-          inputProps={{ "aria-label": "secondary checkbox" }}
-          checked={this.props.checked}
-          onClick={() => this.props.check(this.props.id)}
-        />
-        <p className={this.props.checked? "checked" : ""}>{this.props.content}</p>
-        <Button
-          variant="contained"
-          color="primary"
-          id="btn-danger"
-          onClick={() => this.props.onDel(this.props.id)}
-        >
-          <i className="fas fa-trash-alt"></i>
-        </Button>
-      </div>
+      <Grid item>
+        <Paper elevation={2}>
+          <Grid container alignItems="center" justify="center" >
+            <Grid item sm={1}>
+              <Checkbox
+                color="primary"
+                inputProps={{ "aria-label": "secondary checkbox" }}
+                checked={this.props.checked}
+                onClick={() => this.props.check(this.props.id)}
+              />
+            </Grid>
+            <Grid item sm={10}>
+              <Typography className={this.props.checked ? "checked" : ""} variant="body1">
+                {this.props.content}
+              </Typography>
+            </Grid>
+            <Grid item sm={1}>
+              <IconButton
+                aria-label="delete"
+                color="secondary"
+                onClick={() => this.props.onDel(this.props.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
     );
   }
 }
@@ -33,6 +48,6 @@ Item.propTypes = {
   content: PropTypes.string,
   check: PropTypes.func,
   onDel: PropTypes.func
-}
+};
 
 export default Item;
