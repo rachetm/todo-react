365869730d6fc15617d7ea9e57e15c039fb21948
Rachet Mudnur <rmudnur@gmail.com>
2020-02-19 18:09:20 +0530

    Moved redundant code to a function

--- src/components/Items/items.js
+++ src/components/Items/items.js
@@ -1,25 +1,30 @@
 import React from "react";
 import Item from "./item/item";
 import { connect } from "react-redux";
-import { mark_checked, removeTodo} from "../../actions/actions";
+import { mark_checked, removeTodo } from "../../actions/actions";
 
-function Items(props){
-    let unchecked_items = [];
-    let checked_items = [];
-    const {items=[], filtered_items = []} = props;
-    const itemsToRender = filtered_items.length ? filtered_items : items;
-    
-    itemsToRender.forEach(item => {
-        if (item.checked === true)
-            checked_items.push(
-                <Item key={item.id} {...item} onDel={props.onDel} check={props.check}/>
-            );
-        else
-                unchecked_items.push(
-                    <Item key={item.id} {...item} onDel={props.onDel} check={props.check}/>
-                );
-    })
-    return unchecked_items.concat(checked_items)
+function Items(props) {
+  const unchecked_items = [];
+  const checked_items = [];
+  const { items = [], filtered_items = [], onDel, check} = props;
+  const itemsToRender = filtered_items.length ? filtered_items : items;
+
+  const itemToPush = (item) => {
+    const {id} = item;
+    return <Item key={id} {...item} onDel={onDel} check={check} />;
+  }
+
+  itemsToRender.forEach(item => {
+    if (item.checked)
+      checked_items.push(
+        itemToPush(item)
+      );
+    else
+      unchecked_items.push(
+        itemToPush(item)
+      );
+  });
+  return unchecked_items.concat(checked_items);
 }
 
 const mapStateToProps = state => {
