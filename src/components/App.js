84d6368d3efce7afab79ad78a8f61ef9fb90a0b7
Rachet Mudnur <rmudnur@gmail.com>
2020-02-19 18:10:16 +0530

    Used grid layout and Typography

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,58 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import Add from "./Add";
 import Items from "./Items/items";
 import Search from "./Search";
 import Counter from "./Counter";
 import PropTypes from "prop-types";
-
 class App extends React.Component {
   render() {
     return (
-      <div style={{ height: "100%" }}>
-        <h1>{this.props.title}</h1>
-        <div style={{ display: "flex" }}>
-          <div style={{ flex: "50%" }}>
-            <Add />
-            <Counter />
-          </div>
-          <div style={{ flex: "50%" }}>
-            <div style={{ margin: "auto 30%" }}>
-              <Search />
-            </div>
-            <div
-              style={{
-                margin: "0% 15%",
-                maxHeight: "600px",
-                overflow: "auto",
-                padding: "1%",
-                marginTop: "2%"
-              }}
+      <>
+        <Grid container direction="column">
+          <Typography variant="h1" gutterBottom className="title">
+            Todo
+          </Typography>
+          <Grid container style={{ flex: "1", flexWrap: "nowrap" }}>
+            <Grid
+              container
+              item
+              sm={6}
+              direction="column"
+              style={{ flex: "1" }}
             >
-              <Items />
-            </div>
-          </div>
-        </div>
-      </div>
+              <Add />
+              <Counter />
+            </Grid>
+            <Grid container item sm={6} direction="column" alignItems="center">
+              <Search />
+              <Grid
+                container
+                item
+                direction="column"
+                spacing={2}
+                sm={10}
+                style={{
+                  marginTop: "5%",
+                  overflow: "auto",
+                  flexWrap: "nowrap",
+                  flex: "1 1 1px",
+                  marginBottom: "20px"
+                }}
+              >
+                <Items />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </>
     );
   }
 }
 
 App.propTypes = {
   title: PropTypes.string
-}
+};
 
 export default App;
